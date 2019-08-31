66bd0badf5cca61a04d53f1fa942737ff16a1f85
groott <donrogal@gmail.com>
2019-08-31 16:08:51 +0200

    Added turning left and right #221

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TankMovementComponent.h"
+#include "TankTracks.h"
+
+void UTankMovementComponent::Initilise(UTankTracks* LeftTrackToSet, UTankTracks* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
+
+void UTankMovementComponent::IntendTurnLeft(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(-Throw);
+	RightTrack->SetThrottle(Throw);
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,8 +6,10 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTracks;
+
 /**
- * 
+ * Responsible for driving the tank tracks
  */
 UCLASS( meta = ( BlueprintSpawnableComponent ) )
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
@@ -15,9 +17,20 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
-	
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Initilise(UTankTracks* LeftTrackToSet, UTankTracks* RightTrackToSet);
+
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
 
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
 
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnLeft(float Throw);
+
+private:
+
+	UTankTracks* LeftTrack = nullptr;
+	UTankTracks* RightTrack = nullptr;
 };
